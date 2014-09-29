356623b01edf3314074aa10a13927248c8641096
Victor Sollerhed <victor.sollerhed@ticnet.se>
2014-09-29 15:47:22 +0200

    Increaded Google Analytics statistics tracking.

--- index.php
+++ index.php
@@ -151,6 +151,7 @@ foreach ($audioFiles as $audioFile) {
 	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 	ga('create', 'UA-1094632-2', 'auto');
+	ga('require', 'displayfeatures');
 	ga('send', 'pageview');
 
 </script>
